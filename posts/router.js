be0e96244e08523d164efe2e2e22c771418cc4ee
danielkim713 <danielkim713@gmail.com>
2018-02-24 11:58:45 -0800

    edits to post

--- posts/router.js
+++ posts/router.js
@@ -18,6 +18,62 @@ router.post('/', jwtAuth, (req, res) => {
   res.json({authToken});
 });
 
+// router.post('/', jsonParser, (req,res) => {
+// 	const requiredFields = ['username', 'content', 'country'];
+// 	const missingField = requiredField.find(field => !(fiend in req.body));
+
+// 	if (missingField) {
+// 		return res.status(422).json({
+// 			code: 422;
+// 			reason: 'CompletionError',
+// 			message: 'Missing field', 
+// 			lcation: missingField
+// 		});
+// 	}
+
+// 	const stringField = ['username', 'content', 'country'];
+// 	const nonStringField = stringFields.find(
+// 		field => field in req.body && typeof req.body[field] !== 'string'
+// 	);
+
+// 	if (nonStringField) {
+// 		return res.status(422).json({
+// 			code: 422,
+// 			reason: 'CompletionError',
+// 			message: 'Incorrect field type: expected string',
+// 			location: nonStringField
+// 		});
+// 	}
+
+// 	const sizedFields = {
+// 		content: {
+// 			min: 1
+// 		}
+// 	};
+
+// 	const tooSmallField = Object.keys(sizedFields).find(
+// 		field =>
+//       		'min' in sizedFields[field] &&
+//             	req.body[field].trim().length < sizedFields[field].min
+//   	);
+
+//   	if tooSmallField {
+//   		return res.status(422).json({
+//   			code: 422, 
+//   			reason: 'ValidationError',
+//   			message: tooSmallField
+//   				?`Must be at least ${sizedFields[tooSmallField]
+//           		.min} characters long`,
+//           	location: tooSmallField
+//   		});
+//   	}
+
+//   	let {username, content, country} = req.body;
+
+//   	return Users
+
+//});
+
 router.get('/:country', jwtAuth, (req, res) => {
 	let country = req.params.country;
 
